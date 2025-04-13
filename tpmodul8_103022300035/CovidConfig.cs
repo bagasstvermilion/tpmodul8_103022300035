583aeb94a9b22466baf2273e15c1376f27a37b00
Bagas Pratama <bagassss.com@gmail.com>
2025-04-14 00:54:57 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_103022300035/CovidConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_103022300035
+{
+    class CovidConfig
+    {
+        public string satuan_suhu { get; set; } = "celcius";
+        public int batas_hari_demam { get; set; } = 14;
+        public string pesan_ditolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        public string pesan_diterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+        private const string CONFIG_PATH = @"../../../covid_config.json";
+
+        public static CovidConfig LoadConfig()
+        {
+            if (!File.Exists(CONFIG_PATH))
+            {
+                var defaultConfig = new CovidConfig();
+                defaultConfig.SaveConfig();
+                return defaultConfig;
+            }
+
+            string json = File.ReadAllText(CONFIG_PATH);
+            return JsonSerializer.Deserialize<CovidConfig>(json);
+        }
+
+        public void SaveConfig()
+        {
+            string json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(CONFIG_PATH, json);
+        }
+
+        public void UbahSatuan()
+        {
+            satuan_suhu = satuan_suhu == "celcius" ? "fahrenheit" : "celcius";
+            SaveConfig();
+        }
+    }
+}
--- tpmodul8_103022300035/Program.cs
+++ tpmodul8_103022300035/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tpmodul8_103022300035;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = CovidConfig.LoadConfig();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam satuan {config.satuan_suhu}:");
+        double suhu = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hariDemam = int.Parse(Console.ReadLine());
+
+        bool suhuNormal = (config.satuan_suhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) ||
+                           (config.satuan_suhu == "fahrenheit" && suhu >= 97.7 && suhu <= 99.5);
+
+        if (suhuNormal && hariDemam < config.batas_hari_demam)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+
+        config.UbahSatuan();
+        Console.WriteLine($"Satuan suhu telah diubah ke {config.satuan_suhu}.");
+    }
+}
